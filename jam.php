01fe92b0c44105d4185f7448478f20970d7476c9
daffasetiawan <daffa1700018027@webmail.uad.ac.id>
2018-06-08 00:58:40 +0700

    PWeb2017-A-1700018027--Daffa-Setiawan-Suparno

---
+++ jam.php
@@ -0,0 +1,420 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>KANTIN SEHAT</title>
+	<link rel="stylesheet" type="text/css" href="tersera.css">
+	<script type="text/javascript">
+		function hitung() {
+		var Makanan= parseFloat(document.fform.Makanan.value); 
+		var Minuman= parseFloat(document.fform.Minuman.value);
+		var Makanan1= parseFloat(document.fform.Makanan1.value); 
+		var Minuman1= parseFloat(document.fform.Minuman1.value);
+		var total = 0.0;
+		var biaya = 0.0;
+		var harga = 0.0;
+		if (Makanan==1) {
+			harga = 10000;
+			biaya = harga * Makanan1;
+			total = total + biaya;
+			biaya=0.0;
+			if (Minuman==1) {
+				harga = 15000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==2) {
+				harga = 7000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==3) {
+				harga = 10000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==4) {
+				harga = 8000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==5) {
+				harga = 9000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==6) {
+				harga = 7000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==7) {
+				harga = 9000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+		}
+		else if (Makanan==2) {
+			harga = 10000;
+			biaya = harga * Makanan1;
+			total = total + biaya;
+			biaya=0.0;
+			if (Minuman==1) {
+				harga = 15000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==2) {
+				harga = 7000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==3) {
+				harga = 10000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==4) {
+				harga = 8000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==5) {
+				harga = 9000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==6) {
+				harga = 7000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==7) {
+				harga = 9000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+		}  
+		else if (Makanan==3) {
+			harga = 15000;
+			biaya = harga * Makanan1;
+			total = total + biaya;
+			biaya=0.0;
+			if (Minuman==1) {
+				harga = 15000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==2) {
+				harga = 7000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==3) {
+				harga = 10000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==4) {
+				harga = 8000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==5) {
+				harga = 9000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==6) {
+				harga = 7000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==7) {
+				harga = 9000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+		}
+		else if (Makanan==4) {
+			harga = 13000;
+			biaya = harga * Makanan1;
+			total = total + biaya;
+			biaya=0.0;
+			if (Minuman==1) {
+				harga = 15000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==2) {
+				harga = 7000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==3) {
+				harga = 10000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==4) {
+				harga = 8000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==5) {
+				harga = 9000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==6) {
+				harga = 7000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==7) {
+				harga = 9000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+		}
+		else if (Makanan==5) {
+			harga = 20000;
+			biaya = harga * Makanan1;
+			total = total + biaya;
+			biaya=0.0;
+			if (Minuman==1) {
+				harga = 15000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==2) {
+				harga = 7000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==3) {
+				harga = 10000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==4) {
+				harga = 8000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==5) {
+				harga = 9000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==6) {
+				harga = 7000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==7) {
+				harga = 9000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+		}
+		else if (Makanan==6) {
+			harga = 21000;
+			biaya = harga * Makanan1;
+			total = total + biaya;
+			biaya=0.0;
+			if (Minuman==1) {
+				harga = 15000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==2) {
+				harga = 7000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==3) {
+				harga = 10000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==4) {
+				harga = 8000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==5) {
+				harga = 9000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==6) {
+				harga = 7000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==7) {
+				harga = 9000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+		}
+		else if (Makanan==7) {
+			harga = 35000;
+			biaya = harga * Makanan1;
+			total = total + biaya;
+			biaya=0.0;
+			if (Minuman==1) {
+				harga = 15000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==2) {
+				harga = 7000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==3) {
+				harga = 10000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==4) {
+				harga = 8000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==5) {
+				harga = 9000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==6) {
+				harga = 7000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+			else if (Minuman==7) {
+				harga = 9000;
+				biaya = harga * Minuman1;
+				total = total + biaya;
+				alert("Total Biaya : " +total);
+			}
+		}
+	}
+	</script>
+</head>
+<body>
+	<p id="Judul"> <marquee> KANTIN SEHAT PEMROGRAMAN WEB </marquee> </p>
+	<div id="tersera">
+	<table width="100%" id="tabelku" border="1">
+<form name="fform" >
+		<tr>
+			<td class="judul2">Makanan</td>
+			<td class="judul2">Minuman</td>
+		</tr>
+		<tr>
+			<td>1.Sosis Bakar : Rp.10.000</td>
+			<td>1.Jus Buah Naga : Rp.15.000</td>
+		</tr>
+		<tr>
+			<td>2.Nasi Goreng : Rp.10.000</td>
+			<td>2.Jus Apel : Rp.7.000</td>
+		</tr>
+		<tr>
+			<td>3.Mie Goreng : Rp.15.000</td>
+			<td>3.Teh Hangat : Rp.10.000</td>
+		</tr>
+		<tr>
+			<td>4.Mie Kuah : Rp.13.000</td>
+			<td>4.Es Teh : Rp.8.000</td>
+		</tr>
+		<tr>
+			<td>5.Ayam Goreng : Rp.20.000</td>
+			<td>5.Es Jeruk : Rp.9.000</td>
+		</tr>
+		<tr>
+			<td>6.Ayam Bakar : Rp.21.000</td>
+			<td>6.Jeruk Hangat : Rp.7.000</td>
+		</tr>
+		<tr>
+			<td>7.Bebek Goreng : Rp.35.000</td>
+			<td>7.Capuchino : Rp.9.000</td>
+		</tr>
+		<tr>
+			<td><label>Pilih Makanan : </label>
+				<input type="text" name="Makanan"></td>
+			<td><label>Pilih Minuman :</label>
+				<input type="text" name="Minuman"></td>
+		</tr>
+		<tr>
+			<td>
+				<label>Jumlah Pesan Makan</label>
+				<input type="text" name="Makanan1"></td>
+			<td><label>Jumlah Pesan Minum</label>
+				<input type="text" name="Minuman1"></td>
+		</tr>
+		<tr>
+			<td colspan="2" ><input type="button" name="submit" value="Hitung Total Biaya" onclick="hitung()"></td>
+		</tr>
+</form>
+	</table>
+	</div>
+	<p id="Judul3"> Design by Daffa Setiawan Suparno 1700018027 </p>
+	<p id="Judul3"> Contact Person : 
+	<br> Phone Number : 085796541076 
+<a href="https://www.instagram.com/daffasetiawann/"><img  src="https://sguru.org/wp-content/uploads/2018/01/instagram-colourful-icon.png" width="4%" style="float: right;"></a>
+
+<a href="https://www.facebook.com/daffa.spentil"><img  src="http://www.cercainsubria.com/wp-content/uploads/fb-icon-page-2-facebook-logo-png-transparent-background-1.png" width="5%" style="float: right;"></a>
+	</br>
+</p>
+
+</body>
+</html>
